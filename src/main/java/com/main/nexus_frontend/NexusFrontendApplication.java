ff8e21eb42a1e819934a66d0d977a8794e54e0da
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-06-15 10:10:06 -0300

    initial commit

---
+++ src/main/java/com/main/nexus_frontend/NexusFrontendApplication.java
@@ -0,0 +1,13 @@
+package com.main.nexus_frontend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class NexusFrontendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(NexusFrontendApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/main/nexus_frontend/NexusFrontendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.main.nexus_frontend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NexusFrontendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
