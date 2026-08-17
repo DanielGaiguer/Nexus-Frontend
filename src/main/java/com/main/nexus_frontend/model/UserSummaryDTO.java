a1824be5b0c5a89bf7548af1f0a96e5f375dd0a1
Daniel Gaiguer <gaiguer2019@gmail.com>
2026-08-17 10:30:02 -0300

    ajust style

--- src/main/java/com/main/nexus_frontend/model/UserSummaryDTO.java
+++ src/main/java/com/main/nexus_frontend/model/UserSummaryDTO.java
@@ -6,6 +6,7 @@ public class UserSummaryDTO {
     private String email;
     private String type;
     private Boolean active;
+    private String profilePhotoUrl;
 
     public UserSummaryDTO() {}
 
@@ -19,4 +20,6 @@ public class UserSummaryDTO {
     public void setType(String type) { this.type = type; }
     public Boolean getActive() { return active; }
     public void setActive(Boolean active) { this.active = active; }
+    public String getProfilePhotoUrl() { return profilePhotoUrl; }
+    public void setProfilePhotoUrl(String profilePhotoUrl) { this.profilePhotoUrl = profilePhotoUrl; }
 }
